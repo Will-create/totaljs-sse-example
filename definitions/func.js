da3c918b8d3bbbd1c810c78071d562dfb52234b5
Louis Bertson <louisbertson@gmail.com>
2023-11-10 10:02:11 +0000

    First commit

---
+++ definitions/func.js
@@ -0,0 +1,25 @@
+FUNC.patch = async function() {
+	var db = DB();
+
+	var items = await db.find('nosql/items').promise();
+
+	for (m of items) {
+		var id = UID();
+		m.id = id;
+		await db.insert('nosql/items2', m).promise();
+	}
+}
+
+
+FUNC.refresh_messages = function (room) {
+	var db = DB();
+	room.messages.wait(async function(m, next) {
+		var msg = await db.read('nosql/messages').where('id', m.id).promise();
+		if (!msg)
+			await db.insert('nosql/messages', m).promise();
+
+		next();
+	}, function() {
+		console.log('DONE');
+	})
+}
